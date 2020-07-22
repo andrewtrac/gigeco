b7f0c10037bc09a7238897826d20e2d636dacf30
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 19:20:41 -0400

    used useEffect on mounting memberstack

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export default function Registration () {
+  
+  useEffect(() => {
+    window.memberstack.init(); // mount
+    return () => {
+      window.memberstack.destroy(); // unmount
+    }
+  }, [])
 
   return (
     <form data-ms-form="signup">
     </form>
-
   )
 
 }
