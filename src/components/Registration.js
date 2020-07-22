108297a57768443f74f7d9c5b7795d5d67dcd436
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 18:37:58 -0400

    Debugging Registration

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -4,9 +4,6 @@ export default function Registration () {
 
   return (
     <form data-ms-form="signup">
-    <h1>Hello</h1>
-    <p>Enter your name</p>
-    <input type="text" />
     </form>
 
   )
