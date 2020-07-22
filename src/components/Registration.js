b584495527412326369e1e8c13aafbb5ffbceb56
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 18:11:24 -0400

    Created Registration component

---
+++ src/components/Registration.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Registration () {
+
+  return (
+    <form>
+    <h1>Hello</h1>
+    <p>Enter your name</p>
+    <input type="text" />
+    </form>
+
+  )
+
+}
