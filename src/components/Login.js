e11df17f9bf4d93244c54191e74f3d74c57d55f3
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 17:44:07 -0400

    created basic form

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Login () {
+
+  return (
+    <form>
+    <h1>Hello</h1>
+    <p>Enter your name</p>
+    <input type="text" />
+    
+    </form>
+
+  )
+
+}
