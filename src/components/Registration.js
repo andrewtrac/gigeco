4d4f75512b389c6e2f89765ad70f2c6878e848bb
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 18:20:28 -0400

    added Memberstack form

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Registration () {
 
   return (
-    <form>
+    <form data-ms-form="signup">
     <h1>Hello</h1>
     <p>Enter your name</p>
     <input type="text" />
