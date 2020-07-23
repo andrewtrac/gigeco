437d4bbc929bd76e29d0ba7d49feffbf6715e023
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-22 21:58:30 -0400

    useEffect rendering script

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -2,13 +2,15 @@ import React, { useEffect } from 'react'
 
 export default function Registration () {
 
-
   useEffect(() => {
     let script = document.createElement('script');
     script.src = 'https://api.memberstack.io/static/memberstack.js?custom';
     script.setAttribute('data-memberstack-id', '335d53e2477f259d83f9b05ece497148');
     script.async = true;
-    document.body.appendChild(script);
+    script.onload = function () {
+      console.log('Memberstack Loaded')
+    }
+    document.head.appendChild(script);
   }, [])
 
   return (
